e9a680d2fd35b88c74bb3ade88a76fd604476f3c
Lucas Cunha <lucas71cunha@gmail.com>
2024-10-24 13:00:42 -0300

    Clean architeture, padrão builder

--- src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
+++ src/test/java/br/com/alura/codechella/domain/entities/usuario/UsuarioTest.java
@@ -10,5 +10,24 @@ class UsuarioTest {
     public void naoDeveCadastrarUsuarioComCpfNoFormatoInvalido() {
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> new Usuario("123456789-99", "Lucas", LocalDate.parse("1992-06-25"), "email@email.com"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("12345678999", "Lucas", LocalDate.parse("1992-06-25"), "email@email.com"));
+
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> new Usuario("", "Lucas", LocalDate.parse("1992-06-25"), "email@email.com"));
+    }
+
+    @Test
+    public void deveCriarUsuarioUsandoFabricaDeUsuario() {
+        FabricaDeUsuario fabrica = new FabricaDeUsuario();
+        Usuario usuario = fabrica.comNomeCpfNascimento("Lucas", "123.456.789-00",
+                LocalDate.parse("2000-10-01"));
+
+        Assertions.assertEquals("Lucas",usuario.getNome());
+
+        usuario = fabrica.incluiEndereco("09599-999",40,"apto 32");
+
+        Assertions.assertEquals("apto 32", usuario.getEndereco().getComplemento());
     }
 }
